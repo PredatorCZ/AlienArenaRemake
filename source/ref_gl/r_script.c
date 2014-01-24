PredatorCZ/AlienArenaRemake

commit 17be9d2080329627dfd7f02aa0bb6685dc25a01f

RS_Draw now takes an enum that specifies exactly how lightmaps are to be used.
git-svn-id: svn://svn.icculus.org/alienarena/trunk@4060 05fc5d3f-ce2a-0410-9b80-abac23765f80

source/ref_gl/r_script.c
@@ -1309,7 +1309,7 @@ static cvar_t *rs_eval_if_subexpr (rs_cond_val_t *expr)
 }
 
 void RS_Draw (	rscript_t *rs, int lmtex, vec2_t rotate_center, vec3_t normal,
-				qboolean translucent, qboolean separate_lm_texcoords,
+				qboolean translucent, rs_lightmaptype_t lm,
 				void (*draw_callback) (void))
 {
 	vec3_t		vectors[3];
@@ -1346,19 +1346,10 @@ void RS_Draw (	rscript_t *rs, int lmtex, vec2_t rotate_center, vec3_t normal,
 		if (stage->condv && !(rs_eval_if_subexpr(stage->condv)->value))
 			continue; //stage should not execute
 		
-		if (stage->lightmap && !translucent)
-		{
+		if (stage->lightmap && lm != rs_lightmap_off)
 			GL_MBind (1, lmtex);
 			
-			if (separate_lm_texcoords)
-				glUniform1iARB (g_location_rs_lightmap, 2);
-			else
-				glUniform1iARB (g_location_rs_lightmap, 1);
-		}
-		else
-		{
-			glUniform1iARB (g_location_rs_lightmap, 0);
-		}
+		glUniform1iARB (g_location_rs_lightmap, stage->lightmap?lm:0);
 
 		GL_SelectTexture (0);
 		qglPushMatrix ();
@@ -1501,7 +1492,9 @@ void RS_DrawSurface (msurface_t *surf, rscript_t *rs)
 	
 	translucent = (surf->texinfo->flags & (SURF_TRANS33 | SURF_TRANS66)) != 0;
 	
-	RS_Draw (rs, lmtex, rotate_center, surf->plane->normal, translucent, true, BSP_DrawVBOAccum);
+	RS_Draw (	rs, lmtex, rotate_center, surf->plane->normal, translucent,
+				translucent?rs_lightmap_separate_texcoords:rs_lightmap_off,
+				BSP_DrawVBOAccum );
 	
 	BSP_ClearVBOAccum ();
 }

source/ref_gl/r_mesh.c
@@ -688,7 +688,9 @@ void R_Mesh_DrawFrame (int skinnum, qboolean ragdoll, float shellAlpha)
 	    if (currentmodel->lightmap != NULL)
 	        lmtex = currentmodel->lightmap->texnum;
 	    
-		RS_Draw (rs, lmtex, vec3_origin, vec3_origin, false, false, R_Mesh_DrawVBO_Callback);
+		RS_Draw (	rs, lmtex, vec3_origin, vec3_origin, false,
+					lmtex != 0 ? rs_lightmap_on : rs_lightmap_off,
+					R_Mesh_DrawVBO_Callback );
 		
 		return;
 	}

source/ref_gl/r_postprocess.c
@@ -547,7 +547,7 @@ void R_DrawVehicleHUD (void)
 	if(r_shaders->integer && rs)
 	{
 		RS_ReadyScript(rs);
-		RS_Draw (rs, 0, vec3_origin, vec3_origin, false, false, VehicleHud_DrawQuad_Callback);
+		RS_Draw (rs, 0, vec3_origin, vec3_origin, false, rs_lightmap_off, VehicleHud_DrawQuad_Callback);
 	}
 	
 	qglColor4f(1,1,1,1);

source/ref_gl/r_script.h
@@ -183,8 +183,14 @@ void RS_ScanPathForScripts(void);
 int RS_Animate(rs_stage_t *stage);
 void RS_UpdateRegistration(void);
 void RS_SetTexcoords2D (rs_stage_t *stage, float *os, float *ot);
+typedef enum 
+{
+	rs_lightmap_off,
+	rs_lightmap_on,
+	rs_lightmap_separate_texcoords
+} rs_lightmaptype_t;
 void RS_Draw (	rscript_t *rs, int lmtex, vec2_t rotate_center, vec3_t normal,
-				qboolean translucent, qboolean separate_lm_texcoords,
+				qboolean translucent, rs_lightmaptype_t lm,
 				void (*draw_callback) (void));
 void RS_Surface (msurface_t *surf);
 void RS_LoadSpecialScripts(void);

source/ref_gl/r_surf.c
@@ -646,7 +646,8 @@ void R_DrawRSSurfaces (void)
 					BSP_AddSurfToVBOAccum (s);
 				
 				RS_Draw (	rs, gl_state.lightmap_textures + currLMTex,
-							vec3_origin, vec3_origin, false, true,
+							vec3_origin, vec3_origin, false,
+							rs_lightmap_separate_texcoords, 
 							BSP_DrawVBOAccum );
 				
 				BSP_ClearVBOAccum ();

commit 151fc39e860a071806e056c3aa5bf8e263da9c4a

Fix translucent RScript surfaces showing up black. This regression was introduced by r4060.
git-svn-id: svn://svn.icculus.org/alienarena/trunk@4066 05fc5d3f-ce2a-0410-9b80-abac23765f80

source/ref_gl/r_script.c
@@ -1470,7 +1470,7 @@ void RS_DrawSurface (msurface_t *surf, rscript_t *rs)
 	translucent = (surf->texinfo->flags & (SURF_TRANS33 | SURF_TRANS66)) != 0;
 	
 	RS_Draw (	rs, lmtex, rotate_center, surf->plane->normal, translucent,
-				translucent?rs_lightmap_separate_texcoords:rs_lightmap_off,
+				translucent?rs_lightmap_off:rs_lightmap_separate_texcoords,
 				BSP_DrawVBOAccum );
 	
 	BSP_ClearVBOAccum ();