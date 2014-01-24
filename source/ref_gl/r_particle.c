PredatorCZ/AlienArenaRemake

commit dafb5ae801e8cceb6ee9db17a45f98730edddb96

Added new vegetation.
git-svn-id: svn://svn.icculus.org/alienarena/trunk@4052 05fc5d3f-ce2a-0410-9b80-abac23765f80

source/ref_gl/r_particle.c
@@ -1056,7 +1056,7 @@ void R_DrawVegetationSurface ( void )
 			visible = true; //leaves tend to use much larger images, culling results in undesired effects
 			
 			// adjust vertical position, scaled
-			origin[2] += (grass->texsize/32) * grass->size;
+			origin[2] += (grass->texsize/32) * grass->size/(grass->texsize/128);
 		}
 		else // grass
 		{
@@ -1064,7 +1064,7 @@ void R_DrawVegetationSurface ( void )
 			qglDisable( GL_CULL_FACE );
 			
 			// adjust vertical position, scaled
-			origin[2] += (grass->texsize/32) * grass->size;
+			origin[2] += (grass->texsize/32) * grass->size/(grass->texsize/128);
 
 			visible = CM_inPVS_leafs (r_origin_leafnum, grass->leafnum);
 			// TODO: Replace CM_BoxTrace with CM_FastTrace as soon as I write

data1/scripts/maps/ctf-extermination.rscript
@@ -126,9 +126,9 @@ textures/rage/metallight_2
 textures/arena10/cobblestone
 {
 	{
-		map gfx/grass.tga
+		map gfx/weed3.tga
 		scale static 4 static 4
-		colormap 1 1 1
+		colormap .5 .4 .3
 		grass
 		grasstype 0
 	}	

data1/scripts/maps/dm-annihilation.rscript
@@ -53,9 +53,9 @@ textures/arena10/cobblestoneng
 textures/arena10/cobblestone
 {
 	{
-		map gfx/grass.tga
+		map gfx/weed3.tga
 		scale static 4 static 4
-		colormap 1 1 1
+		colormap .5 .4 .3
 		grass
 		grasstype 0
 	}	

data1/scripts/maps/dm-extermination.rscript
@@ -124,9 +124,9 @@ textures/rage/metallight_2
 textures/arena10/cobblestone
 {
 	{
-		map gfx/grass.tga
+		map gfx/weed3.tga
 		scale static 4 static 4
-		colormap 1 1 1
+		colormap .5 .4 .3
 		grass
 		grasstype 0
 	}	

data1/scripts/maps/dm-impact.rscript
@@ -21,9 +21,9 @@ textures/rage/metallight_2
 textures/arena10/cobblestone1
 {
 	{
-		map gfx/grass.tga
+		map gfx/weed3.tga
 		scale static 4 static 4
-		colormap 1 1 1
+		colormap .5 .4 .3
 		grass
 		grasstype 0
 	}
@@ -31,9 +31,9 @@ textures/arena10/cobblestone1
 textures/arena10/rockdirt1
 {
 	{
-		map gfx/grass.tga
+		map gfx/weed3.tga
 		scale static 4 static 4
-		colormap 1 1 1
+		colormap .5 .4 .3
 		grass
 		grasstype 0
 	}

data1/scripts/maps/tac-extermination.rscript
@@ -126,9 +126,9 @@ textures/rage/metallight_2
 textures/arena10/cobblestone
 {
 	{
-		map gfx/grass.tga
+		map gfx/weed3.tga
 		scale static 4 static 4
-		colormap 1 1 1
+		colormap .5 .4 .3
 		grass
 		grasstype 0
 	}	

data1/scripts/maps/tac-impact.rscript
@@ -11,9 +11,9 @@ textures/arena9/roundlight
 textures/arena10/cobblestone1
 {
 	{
-		map gfx/grass.tga
+		map gfx/weed3.tga
 		scale static 4 static 4
-		colormap 1 1 1
+		colormap .5 .4 .3
 		grass
 		grasstype 0
 	}
@@ -21,9 +21,9 @@ textures/arena10/cobblestone1
 textures/arena10/rockdirt1
 {
 	{
-		map gfx/grass.tga
+		map gfx/weed3.tga
 		scale static 4 static 4
-		colormap 1 1 1
+		colormap .5 .4 .3
 		grass
 		grasstype 0
 	}
@@ -60,9 +60,9 @@ textures/rage/metallight_2
 textures/arena10/cobblestone
 {
 	{
-		map gfx/grass.tga
+		map gfx/weed3.tga
 		scale static 4 static 4
-		colormap 1 1 1
+		colormap .5 .4 .3
 		grass
 		grasstype 0
 	}	

data1/scripts/maps/tca-extermination.rscript
@@ -126,9 +126,9 @@ textures/rage/metallight_2
 textures/arena10/cobblestone
 {
 	{
-		map gfx/grass.tga
+		map gfx/weed3.tga
 		scale static 4 static 4
-		colormap 1 1 1
+		colormap .5 .4 .3
 		grass
 		grasstype 0
 	}	

commit 4f29dcb1e7e460ad3b343a655adc13c12dbd7f01

Fix vegetation disappearing under some circumstances. This regression was introduced by r4052.
git-svn-id: svn://svn.icculus.org/alienarena/trunk@4068 05fc5d3f-ce2a-0410-9b80-abac23765f80

source/ref_gl/r_particle.c
@@ -1056,7 +1056,7 @@ void R_DrawVegetationSurface ( void )
 			visible = true; //leaves tend to use much larger images, culling results in undesired effects
 			
 			// adjust vertical position, scaled
-			origin[2] += (grass->texsize/32) * grass->size/(grass->texsize/128);
+			origin[2] += (grass->texsize/32.0) * grass->size/(grass->texsize/128.0);
 		}
 		else // grass
 		{
@@ -1064,7 +1064,7 @@ void R_DrawVegetationSurface ( void )
 			qglDisable( GL_CULL_FACE );
 			
 			// adjust vertical position, scaled
-			origin[2] += (grass->texsize/32) * grass->size/(grass->texsize/128);
+			origin[2] += (grass->texsize/32.0) * grass->size/(grass->texsize/128.0);
 
 			visible = CM_inPVS_leafs (r_origin_leafnum, grass->leafnum);
 			// TODO: Replace CM_BoxTrace with CM_FastTrace as soon as I write