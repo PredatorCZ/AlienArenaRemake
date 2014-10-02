PredatorCZ/AlienArenaRemake

commit a158ba32c267c316609aa9062463a3cd6ebe40ed

Overhaul explosion distortion effects.
The distortions now scale with distance and expand along with the explosion.
The effect's scaling is no longer influenced by screen resolution, and no
longer distorted by different aspect ratios. It is animated by time instead of
by frame count, making it more consistent at different framerates. Only part
of the screen is ever distorted and redrawn, making it more efficient
(although there is a less than ideal hack where the offscreen buffer is screen
sized, which still hurts performance a bit.)

The explosion distortion texture has been changed to make it look more like
ripples.


git-svn-id: svn://svn.icculus.org/alienarena/trunk@4407 05fc5d3f-ce2a-0410-9b80-abac23765f80

source/ref_gl/r_postprocess.c
@@ -41,7 +41,6 @@ vec3_t r_explosionOrigin;
 int r_drawing_fbeffect;
 int	r_fbFxType;
 float r_fbeffectTime;
-int frames;
 
 extern  cvar_t	*cl_raindist;
 
@@ -69,6 +68,8 @@ image_t *R_Postprocess_AllocFBOTexture (char *name, int width, int height, GLuin
 	GL_Bind (image->texnum);
 	qglTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
 	qglTexImage2D (GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	qglTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	qglTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 	
 	// create up the FBO
 	qglGenFramebuffersEXT (1, FBO);
@@ -87,7 +88,7 @@ image_t *R_Postprocess_AllocFBOTexture (char *name, int width, int height, GLuin
 // Be sure to set up your GLSL program and uniforms before calling this! Make
 // sure r_framebuffer is already bound to a TMU, and tell this function which
 // TMU it is.
-static void Distort_RenderQuad (int framebuffer_tmu)
+static void Distort_RenderQuad (int framebuffer_tmu, int x, int y, int w, int h)
 {
 	//set up full screen workspace
 	qglMatrixMode (GL_PROJECTION );
@@ -99,12 +100,14 @@ static void Distort_RenderQuad (int framebuffer_tmu)
 	GLSTATE_DISABLE_BLEND
 	qglDisable (GL_DEPTH_TEST);
 	
+	qglViewport (x, y, w, h);
+	
 	GL_SelectTexture (framebuffer_tmu); // r_framefuffer should already be bound to TMU 0
 	GL_Bind (r_framebuffer->texnum);
 
 	// we need to grab the frame buffer
 	qglBindFramebufferEXT (GL_DRAW_FRAMEBUFFER_EXT, distort_FBO);
-	qglBlitFramebufferEXT(0, 0, viddef.width, viddef.height, 0, 0, viddef.width, viddef.height,
+	qglBlitFramebufferEXT (x, y, x + w, y + h, 0, 0, viddef.width, viddef.height, // HACK!
 		GL_COLOR_BUFFER_BIT, GL_LINEAR);
 	qglBindFramebufferEXT (GL_DRAW_FRAMEBUFFER_EXT, 0);
 	
@@ -113,6 +116,7 @@ static void Distort_RenderQuad (int framebuffer_tmu)
 	GL_SetupWholeScreen2DVBO (wholescreen_fliptextured);
 	R_DrawVarrays (GL_QUADS, 0, 4);
 	R_KillVArrays ();
+	R_SetupViewport ();
 	
 	GLSTATE_ENABLE_BLEND
 	qglEnable (GL_DEPTH_TEST);
@@ -120,7 +124,6 @@ static void Distort_RenderQuad (int framebuffer_tmu)
 
 void R_GLSLDistortion(void)
 {
-	vec2_t fxScreenPos;
 	vec3_t	vec;
 	float	dot, r_fbeffectLen;
 	vec3_t	forward, mins, maxs;
@@ -149,19 +152,13 @@ void R_GLSLDistortion(void)
 	if(!r_drawing_fbeffect)
 		return;
 
-	frames++;
-
-	if(r_fbFxType == PAIN)		
-	{
-		r_fbeffectLen = 0.1;
-		R_DrawBloodEffect();
-	}
-	else
-		r_fbeffectLen = 0.2;
-
 	if(r_fbFxType == EXPLOSION)
 	{
-		int i;
+		vec3_t ul_3d, lr_3d;
+		vec2_t ul_2d, lr_2d;
+		float scale, intensity;
+		
+		r_fbeffectLen = 0.2f;
 		
 		//create a distortion wave effect at point of explosion
 		glUseProgramObjectARB( g_fbprogramObj );
@@ -175,27 +172,29 @@ void R_GLSLDistortion(void)
 			GL_MBind (0, r_distortwave->texnum);
 		glUniform1iARB (distort_uniforms.distortTex, 0);
 
-		fxScreenPos[0] = fxScreenPos[1] = 0;
+		// get positions of bounds of warp area
+		scale = 8.0f + (rs_realtime - r_fbeffectTime) * 78.0f / r_fbeffectLen;
+		VectorMA (r_explosionOrigin, -scale, vright, ul_3d);
+		VectorMA (ul_3d, scale, vup, ul_3d);
+		R_TransformVectorToScreen(&r_newrefdef, ul_3d, ul_2d);
+		VectorMA (r_explosionOrigin, scale, vright, lr_3d);
+		VectorMA (lr_3d, -scale, vup, lr_3d);
+		R_TransformVectorToScreen(&r_newrefdef, lr_3d, lr_2d);
 
-		//get position of focal point of warp
-		R_TransformVectorToScreen(&r_newrefdef, r_explosionOrigin, fxScreenPos);
-
-		fxScreenPos[0] /= viddef.width; 
-		fxScreenPos[1] /= viddef.height;
-		
-		for (i = 0; i < 2; i++)
-			fxScreenPos[i] -= 0.5f + (float)frames * 0.001f;
-		
-		glUniform2fARB (distort_uniforms.fxPos, fxScreenPos[0], fxScreenPos[1]);
+		intensity = sinf (M_PI * (rs_realtime - r_fbeffectTime) / r_fbeffectLen);
+		glUniform1fARB (distort_uniforms.intensity, intensity);
 		
 		// Note that r_framebuffer is on TMU 1 this time
-		Distort_RenderQuad (1);
+		Distort_RenderQuad (1, ul_2d[0], lr_2d[1], lr_2d[0] - ul_2d[0], ul_2d[1] - lr_2d[1]);
 		
 		glUseProgramObjectARB (0);
 		GL_MBind (1, 0);
 	}
 	else
 	{
+		r_fbeffectLen = 0.1f;
+		R_DrawBloodEffect();
+		
 		//do a radial blur
 		glUseProgramObjectARB( g_rblurprogramObj );
 
@@ -205,16 +204,13 @@ void R_GLSLDistortion(void)
 
 		glUniform3fARB( g_location_rparams, viddef.width/2.0, viddef.height/2.0, 0.25);
 
-		Distort_RenderQuad (0);
+		Distort_RenderQuad (0, 0, 0, viddef.width, viddef.height);
 
 		glUseProgramObjectARB( 0 );
 	}
 
 	if(rs_realtime > r_fbeffectTime+r_fbeffectLen) 
-	{
-		frames = 0;
 		r_drawing_fbeffect = false; //done effect
-	}
 
 	return;
 }
@@ -253,7 +249,7 @@ void R_GLSLWaterDroplets(void)
 
 	glUniform1fARB( g_location_drTime, rs_realtime);
 	
-	Distort_RenderQuad (0);
+	Distort_RenderQuad (0, 0, 0, viddef.width, viddef.height);
 
 	glUseProgramObjectARB( 0 );
 

source/ref_gl/r_program.c
@@ -1653,30 +1653,17 @@ static char fb_vertex_program[] = STRINGIFY (
 static char fb_fragment_program[] = STRINGIFY (
 	uniform sampler2D fbtexture;
 	uniform sampler2D distortiontexture;
-	uniform vec2 fxPos;
+	uniform float intensity;
 
 	void main(void)
 	{
-		vec3 noiseVec;
+		vec2 noiseVec;
 		vec2 displacement;
 		
-		displacement = gl_TexCoord[0].st - fxPos;
-
-		noiseVec = normalize(texture2D(distortiontexture, displacement.xy)).xyz;
-		noiseVec = (noiseVec * 2.0 - 0.635) * 0.035;
-
-		//clamp edges to prevent artifacts
-		if(gl_TexCoord[0].s > 0.1 && gl_TexCoord[0].s < 0.992)
-			displacement.x = gl_TexCoord[0].s + noiseVec.x;
-		else
-			displacement.x = gl_TexCoord[0].s;
-
-		if(gl_TexCoord[0].t > 0.1 && gl_TexCoord[0].t < 0.992) 
-			displacement.y = gl_TexCoord[0].t + noiseVec.y;
-		else
-			displacement.y = gl_TexCoord[0].t;
-
-		gl_FragColor = texture2D (fbtexture, displacement.xy);
+		noiseVec = normalize(texture2D(distortiontexture, gl_TexCoord[0].st)).xy;
+		displacement = gl_TexCoord[0].st + (noiseVec * 2.0 - vec2 (0.6389, 0.6339)) * intensity;
+		
+		gl_FragColor = texture2D (fbtexture, displacement);
 	}
 );
 
@@ -2237,7 +2224,7 @@ void R_LoadGLSLPrograms(void)
 	// Locate some parameters by name so we can set them later...
 	distort_uniforms.framebuffTex = glGetUniformLocationARB (g_fbprogramObj, "fbtexture");
 	distort_uniforms.distortTex = glGetUniformLocationARB (g_fbprogramObj, "distorttexture");
-	distort_uniforms.fxPos = glGetUniformLocationARB (g_fbprogramObj, "fxPos");
+	distort_uniforms.intensity = glGetUniformLocationARB (g_fbprogramObj, "intensity");
 
 	//gaussian blur
 	R_LoadGLSLProgram ("Framebuffer Blur", (char*)blur_vertex_program, (char*)blur_fragment_program, NO_ATTRIBUTES, &g_blurprogramObj);

source/ref_gl/r_local.h
@@ -699,7 +699,7 @@ mesh_anim_uniform_location_t blankmesh_uniforms;
 struct
 {
 	GLuint	framebuffTex, distortTex;
-	GLuint	fxPos;
+	GLuint	intensity;
 } distort_uniforms;
 
 //gaussian blur

commit 5a3676b849fbb2b9e964166c550b6b6e1f018f3a

Fix explosion distortion when MSAA is enabled. This fixes a regression introduced in r4407.
git-svn-id: svn://svn.icculus.org/alienarena/trunk@4409 05fc5d3f-ce2a-0410-9b80-abac23765f80

source/ref_gl/r_postprocess.c
@@ -90,34 +90,50 @@ image_t *R_Postprocess_AllocFBOTexture (char *name, int width, int height, GLuin
 // TMU it is.
 static void Distort_RenderQuad (int framebuffer_tmu, int x, int y, int w, int h)
 {
+	int xl = x, yt = y, xr = x + w, yb = y + h;
+	
 	//set up full screen workspace
 	qglMatrixMode (GL_PROJECTION );
-    qglLoadIdentity ();
+	qglLoadIdentity ();
 	qglOrtho (0, 1, 1, 0, -10, 100);
-	qglMatrixMode( GL_MODELVIEW );
-    qglLoadIdentity ();
-    
+	qglMatrixMode (GL_MODELVIEW);
+	qglLoadIdentity ();
+	
 	GLSTATE_DISABLE_BLEND
 	qglDisable (GL_DEPTH_TEST);
 	
-	qglViewport (x, y, w, h);
-	
 	GL_SelectTexture (framebuffer_tmu); // r_framefuffer should already be bound to TMU 0
 	GL_Bind (r_framebuffer->texnum);
-
-	// we need to grab the frame buffer
+	
+	// Ie need to grab the frame buffer. If we're not working with the whole
+	// framebuffer, copy a margin to avoid artifacts
 	qglBindFramebufferEXT (GL_DRAW_FRAMEBUFFER_EXT, distort_FBO);
-	qglBlitFramebufferEXT (x, y, x + w, y + h, 0, 0, viddef.width, viddef.height, // HACK!
-		GL_COLOR_BUFFER_BIT, GL_LINEAR);
+	if (xl > 0) xl--;
+	if (xr < viddef.width) xr++;
+	if (yt > 0) yt--;
+	if (yb < viddef.height) yb++;
+	qglBlitFramebufferEXT (xl, yt, xr, yb, xl, yt, xr, yb, GL_COLOR_BUFFER_BIT, GL_LINEAR);
 	qglBindFramebufferEXT (GL_DRAW_FRAMEBUFFER_EXT, 0);
+	
+	qglViewport (x, y, w, h);
+	
+	// Set the texture matrix so the GLSL shader knows only to sample part
+	// of the FB texture
+	GL_SelectTexture (0);
+	qglMatrixMode (GL_TEXTURE);
+	qglPushMatrix ();
+	qglTranslatef ((float)x/(float)viddef.width, (float)y/(float)viddef.height, 0);
+	qglScalef ((float)w/(float)viddef.width, (float)h/(float)viddef.height, 1);
 	
 	// FIXME: textures captured from the framebuffer need to be rendered
 	// upside down for some reason
 	GL_SetupWholeScreen2DVBO (wholescreen_fliptextured);
 	R_DrawVarrays (GL_QUADS, 0, 4);
+	
+	qglPopMatrix ();
+	qglMatrixMode (GL_MODELVIEW);
 	R_KillVArrays ();
 	R_SetupViewport ();
-	
 	GLSTATE_ENABLE_BLEND
 	qglEnable (GL_DEPTH_TEST);
 }
@@ -180,7 +196,7 @@ void R_GLSLDistortion(void)
 		VectorMA (r_explosionOrigin, scale, vright, lr_3d);
 		VectorMA (lr_3d, -scale, vup, lr_3d);
 		R_TransformVectorToScreen(&r_newrefdef, lr_3d, lr_2d);
-
+		
 		intensity = sinf (M_PI * (rs_realtime - r_fbeffectTime) / r_fbeffectLen);
 		glUniform1fARB (distort_uniforms.intensity, intensity);
 		
@@ -383,7 +399,7 @@ void R_FB_InitTextures( void )
 	byte	*data;
 	int		size;
 
-    //init the various FBO textures
+	//init the various FBO textures
 	size = viddef.width * viddef.height * 4;
 	if (size < 16 * 16 * 4) // nullpic min size
 		size = 16 * 16 * 4;

source/ref_gl/r_program.c
@@ -1658,12 +1658,14 @@ static char fb_fragment_program[] = STRINGIFY (
 	void main(void)
 	{
 		vec2 noiseVec;
-		vec2 displacement;
+		vec4 displacement;
 		
 		noiseVec = normalize(texture2D(distortiontexture, gl_TexCoord[0].st)).xy;
-		displacement = gl_TexCoord[0].st + (noiseVec * 2.0 - vec2 (0.6389, 0.6339)) * intensity;
 		
-		gl_FragColor = texture2D (fbtexture, displacement);
+		// It's a bit of a rigomorole to do partial screen updates. This can
+		// go away as soon as the distort texture is a screen sized buffer.
+		displacement = gl_TexCoord[0] + vec4 ((noiseVec * 2.0 - vec2 (0.6389, 0.6339)) * intensity, 0, 0);
+		gl_FragColor = texture2D (fbtexture, (gl_TextureMatrix[0] * displacement).st);
 	}
 );
 