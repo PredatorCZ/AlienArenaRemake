PredatorCZ/AlienArenaRemake

commit 2d86e2884f9be19185586f7f41a2422d4a0a6fe4

Fix SURF_FLOWING rscript surfaces. Also, fix possible SURF_FLOWING state leak.
git-svn-id: svn://svn.icculus.org/alienarena/trunk@4420 05fc5d3f-ce2a-0410-9b80-abac23765f80

source/ref_gl/r_surf.c
@@ -403,6 +403,7 @@ BSP_SetScrolling
 */
 void BSP_SetScrolling (qboolean enable)
 {
+	GL_SelectTexture (0);
 	qglMatrixMode (GL_TEXTURE);
 	qglLoadIdentity ();
 	if (enable)
@@ -613,6 +614,8 @@ static void R_DrawRSSurfaces (void)
 			continue;
 		}
 		
+		BSP_SetScrolling ((currentmodel->unique_texinfo[i]->flags & SURF_FLOWING) != 0);
+		
 		rs = currentmodel->unique_texinfo[i]->image->script;
 		
 		if (rs == NULL || (rs->flags & RS_PREVENT_BATCH) != 0)
@@ -642,6 +645,8 @@ static void R_DrawRSSurfaces (void)
 			}
 		}
 	}
+	
+	BSP_SetScrolling (0);
 	
 	RS_End_Group ();
 	R_KillVArrays ();

commit 484f145933bc61cfccf4ae26967d7c0105b5f30a

Somewhat hacky fix for regression introduced in r4420
git-svn-id: svn://svn.icculus.org/alienarena/trunk@4421 05fc5d3f-ce2a-0410-9b80-abac23765f80

source/ref_gl/r_script.c
@@ -1191,7 +1191,7 @@ static void RS_CleanupGLState (void)
 	GLSTATE_DISABLE_TEXGEN
 }
 
-static qboolean rs_in_group = false;
+qboolean rs_in_group = false;
 static qboolean rs_dlights_enabled;
 
 // If you're about to draw a huge number of RScript surfaces in a row with

source/ref_gl/r_surf.c
@@ -401,10 +401,12 @@ msurface_t	*r_flicker_surfaces;
 BSP_SetScrolling
 ================
 */
-void BSP_SetScrolling (qboolean enable)
+static void BSP_SetScrolling (qboolean enable)
 {
+	extern qboolean rs_in_group; // Sigh
 	GL_SelectTexture (0);
-	qglMatrixMode (GL_TEXTURE);
+	if (!rs_in_group)
+		qglMatrixMode (GL_TEXTURE);
 	qglLoadIdentity ();
 	if (enable)
 	{
@@ -414,7 +416,8 @@ void BSP_SetScrolling (qboolean enable)
 			scroll = -64.0;
 		qglTranslatef (scroll, 0, 0);
 	}
-	qglMatrixMode (GL_MODELVIEW);
+	if (!rs_in_group)
+		qglMatrixMode (GL_MODELVIEW);
 }
 
 

source/ref_gl/r_warp.c
@@ -210,7 +210,6 @@ EmitWaterPolys
 Does a water warp on the pre-fragmented glpoly_t chain
 =============
 */
-void BSP_SetScrolling (qboolean enable);
 void R_RenderWaterPolys (msurface_t *fa)
 {
 	rscript_t	*rs_shader;